be2d49c4a3b30a5b19380f29cdbfea8a9ddccb62
Marcelo W Filho <marcelo.filho@volvo.com>
2025-08-13 18:59:50 -0300

    Add-Redis

--- QueryMind.Foundation/02-database-configuration.tf
+++ QueryMind.Foundation/02-database-configuration.tf
@@ -3,7 +3,7 @@ module "cms_01" {
   location            = azurerm_resource_group.rg_01.location
   resource_group_name = azurerm_resource_group.rg_01.name
   cosmos_account_name = "cms-querymind-${var.env_suffix}"
-  database_name       = var.database_name
+  database_name       = "queryminddb${var.env_suffix}"
 
   collections = {
     Users = {
--- QueryMind.Foundation/03-storage-configuration.tf
+++ QueryMind.Foundation/03-storage-configuration.tf
@@ -13,3 +13,23 @@ resource "azurerm_storage_container" "scripts" {
   storage_account_id    = module.sa_01.storage_account_id
   container_access_type = "blob"
 }
+
+module "arc_01" {
+  source              = "../QueryMind.Skeletons/redis"
+  name                = "arc-querymind-${var.env_suffix}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_01.name
+  sku_name            = "Basic"
+  tags                = var.tags
+}
+
+# resource "azurerm_redis_firewall_rule" "arc_adm_rule" {
+#   name                = "adm_ip"
+#   resource_group_name = azurerm_resource_group.rg_01.name
+#   redis_cache_name    = azurerm_redis_cache.arc_01.name
+#   start_ip            = "MEU_IP"
+#   end_ip              = "MEU_IP"
+# }
+
+# az redis list-keys --name redis_name --resource-group group_name
+# arc-redis-test.redis.cache.windows.net:6380,password=REDIS_KEY=,ssl=True,abortConnect=False
